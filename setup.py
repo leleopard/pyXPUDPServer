ae64b3e2d5234dfed2c96ddc5fa99032369344e4
leleopard <stephaneteisserenc@gmail.com>
2020-03-15 15:07:09 +0000

    removed a print statement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='pyxpudpserver',
-      version='1.2.3',
+      version='1.2.4',
       description='Python class that allows to communicate with XPlane via UDP: Set/receive datarefs, send commands, redirect traffic to XP',
       url='https://github.com/leleopard/pyXPUDPServer',
       author='Stephane Teisserenc',
